3a251bf2cf30387bdfce2efd9fd846e35c6a1159
tralac <paul.apostel@gmail.com>
2012-01-19 09:23:26 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tralac::Application.config.secret_token = '144fc652d7a3d207b00b45c6569298841e2deda80894a78808ef0627d7c0bbe9f03561a0fed76e9de9d49671c58ccefc3e8d292e84e95ce0bea995448e1b8ef7'
